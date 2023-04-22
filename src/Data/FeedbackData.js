daf7fa45f4b6dc995bb8d8cb62c58007334f33f5
Sonila Mohanty <48008901+Sonilakitu@users.noreply.github.com>
2023-04-22 23:46:18 +0530

    Update FeedbackData.js

--- src/Data/FeedbackData.js
+++ src/Data/FeedbackData.js
@@ -15,5 +15,4 @@ const FeedbackData = [
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
 ]
-
 export default FeedbackData;
