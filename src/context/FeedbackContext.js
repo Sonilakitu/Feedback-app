2bffde13fc62e62364b2fa79df21c327cd528a9c
Sonila Mohanty <48008901+Sonilakitu@users.noreply.github.com>
2023-06-09 10:00:38 +0530

    Update FeedbackContext.js

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -4,21 +4,7 @@ const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
-    // {
-    //   id: 1,
-    //   text: "This is feedback item 1",
-    //   rating: 10,
-    // },
-    // {
-    //   id: 2,
-    //   text: "This is feedback item 2",
-    //   rating: 9,
-    // },
-    // {
-    //   id: 3,
-    //   text: "This is feedback item 3",
-    //   rating: 7,
-    // },
+    
   ]);
   const [isLoading, setIsLoading] = useState(true);
   const [feedbackEdit, setFeedbackEdit] = useState({
